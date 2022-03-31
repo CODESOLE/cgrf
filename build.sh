37f51ddb2b5d9745fc0bcfab3a54701fd17de9da
CODESOLE <22984206+CODESOLE@users.noreply.github.com>
2022-04-01 02:14:46 +0300

    some build script changes

--- build.sh
+++ build.sh
@@ -2,8 +2,8 @@
 
 if [ "$1" = "r" ]; then
   echo ==================RELEASE================== && mkdir -p build
-  premake5 gmake2 && cd build/ && make config=release_x64
+  premake5 gmake2 && make -C build config=release_x64
 else
   echo ===================DEBUG=================== && mkdir -p build
-  premake5 gmake2 && cd build/ && make config=debug_x64
+  premake5 gmake2 && make -C build config=debug_x64
 fi
