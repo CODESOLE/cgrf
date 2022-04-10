8cc900713ef0429cc997a51fa6d6955d3aacb62e
CODESOLE <22984206+CODESOLE@users.noreply.github.com>
2022-04-10 23:57:24 +0300

    some update

--- build.sh
+++ build.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
 
 if [ "$1" = "r" ]; then
-  echo ==================RELEASE================== && mkdir -p build
   premake5 gmake2 && make -C build config=release
 else
-  echo ===================DEBUG=================== && mkdir -p build
   premake5 gmake2 && make -C build config=debug
 fi
--- premake5.lua
+++ premake5.lua
@@ -10,14 +10,14 @@ project "cgrf"
   targetname "cgrf"
   targetdir "build"
 
+  os.mkdir("build")
   os.copyfile("tests/test.cgrf", "build")
   os.copyfile("tests/test_with_commented_lines.cgrf", "build")
+
   buildoptions { "-pedantic" }
 
   includedirs { "src", "dep" }
-
   links { "SDL2main", "SDL2", "m" }
-
   files { "src/**.c", "dep/**.c" }
 
   filter "configurations:Debug"
