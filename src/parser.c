24cdb2e14aaf2de42d77a27bd8f983b51b3e9a13
CODESOLE <22984206+CODESOLE@users.noreply.github.com>
2022-04-19 02:37:18 +0300

    _check_extension moved to parser.c from render.c
    And removed some unnecessary duplication removing code

--- src/parser.c
+++ src/parser.c
@@ -78,13 +78,39 @@ static void _tokenize(char *haystack, char *needle) {
   }
 }
 
-static inline _Bool _check_extension(const char *filename) {
-  return strncmp(filename + strlen(filename) - 5, ".cgrf", 5) == 0 ? 0 : 1;
+static inline _Bool _check_extension(const char *filename, const char *ext) {
+  return strncmp(filename + strlen(filename) - strlen(ext), ext, strlen(ext)) ==
+                 0
+             ? 0
+             : 1;
+}
+
+static void
+_check_remove_duplicated_and_revese_pairs(struct array_str_s *toks) {
+  char tmp[512] = {0};
+  char tmp2[512] = {0};
+  for (size_t i = 0; i < array_str_size(toks); i += 2) {
+    char *s1 = *array_str_get(toks, i);
+    char *s2 = *array_str_get(toks, i + 1);
+    snprintf(tmp, 512, "%s%s", s1, s2);
+    for (size_t j = 0; j < array_str_size(toks); j += 2) {
+      if (j == i)
+        continue;
+      char *ss1 = *array_str_get(toks, j);
+      char *ss2 = *array_str_get(toks, j + 1);
+      snprintf(tmp2, 512, "%s%s", ss1, ss2);
+      if (strncmp(tmp, tmp2, strlen(tmp2)) == 0) {
+        array_str_erase(toks, j);
+        array_str_erase(toks, j);
+      }
+    }
+  }
 }
 
 struct array_str_s *cgrf_parse_file(const char *filename) {
   assert(filename);
-  assert(_check_extension(filename) == 0);
+  CGRF_FAIL_IF_WRONG(_check_extension(filename, ".cgrf") == 0, __FILE__,
+                     __LINE__, exit(EXIT_FAILURE));
   FILE *f = NULL;
   CGRF_FOPEN(f, filename, "r", exit(EXIT_FAILURE), __FILE__, __LINE__);
   char line[MAX_CHAR];
@@ -99,14 +125,13 @@ struct array_str_s *cgrf_parse_file(const char *filename) {
   for (size_t i = 0; i < array_str_size(t); i += 2) {
     char *s1 = *array_str_get(t, i);
     char *s2 = *array_str_get(t, i + 1);
-    if (strncmp(s1, s2, strlen(s2)) == 0) {
-      if (i == 0)
-        array_str_erase(t, SDL_min(0, i - 1));
-      else
-        array_str_erase(t, i - 1);
+    if (strncmp(s1, s2, strlen(s2)) == 0) { /* removing X--X */
+      array_str_erase(t, i);
       array_str_erase(t, i);
     }
   }
+  /* removing { A--B, B--A } and { A--B, A--B } */
+  _check_remove_duplicated_and_revese_pairs(t);
 
   return t;
 }
